d325f1c9806d7c6181d4b77f41c62b6909ffbe0f
38136 <31004566+38136@users.noreply.github.com>
2017-08-16 10:55:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,46 +6,32 @@ const app = express();
 app.use(bodyParser.urlencoded({
     extended: true
 }));
- app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.post('/', (req, res) => {
     if (req.body.result.action === "greeting") {
 
-//         let json = JSON.parse(body);
-//         let fbUsername = json.first_name;
-        
+        let json = JSON.parse(body);
+        let fbUsername = json.first_name;
+        let sender = event.sender.id;
 
-//          request.get((err, response, body) => {
-//             if (!err && response.statusCode == 200) {
-//                 let textResponse = "hi" + fbUsername + "need to open the account ? Which type Saving or Current Account ?"
+        req.get((err, res, body) => {
+            if (!err && res.statusCode == 200) {
+                let textResponse = "hi" + fbUsername + "need to open the account ? Which type Saving or Current Account ?"
 
-//                 return res.json({
-//                     speech: textResponse,
-//                     display: textResponse,
-//                     source : "greeting"
-//                 });
-//             }
-//             else {
-//                 return res.status(400).json({
-//                     status : {
-//                         code : 400,
-//                         errorType : "Error while binding the username"
-//                     }
-//                 });
-//             }
-//         });
-         
-            if (req.body.result.resolvedQuery == "hi") {
-                let textResponse = "hi, Siri need to open the account ? Which type Saving or Current Account ?"
-                 return res.json({
+                return res.json({
                     speech: textResponse,
                     display: textResponse,
-                    source : "agent"
+                    source : "greeting"
                 });
             }
-     }
-  
+            else {
+                return res.status(400).json({
+                    status : {
+                        code : 400,
+                        errorType : "Error while binding the username"
+                    }
+                });
+            }
+        });
+    }
 });
-
-  app.listen(process.env.PORT || 8080, function (message) {
-    console.log("Server is running on the port...");
-}) 
