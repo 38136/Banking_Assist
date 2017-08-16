f799ed32b9e38848ecdd7e5279a49e57a4599490
38136 <31004566+38136@users.noreply.github.com>
2017-08-16 10:39:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,31 +7,40 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.post('/ai', (req, res) => {
-    if (request.body.result.action === "greeting") {
+app.post('/', (req, res) => {
+    if (req.body.result.action === "greeting") {
 
-        let json = JSON.parse(body);
-        let fbUsername = bodyObj.first_name;
-        let sender = event.sender.id;
+//         let json = JSON.parse(body);
+//         let fbUsername = json.first_name;
+        
 
-        request.get((err, response, body) => {
-            if (!err && response.statusCode == 200) {
-                let textResponse = "hi" + fbUsername + "need to open the account ? Which type Saving or Current Account ?"
+//          request.get((err, response, body) => {
+//             if (!err && response.statusCode == 200) {
+//                 let textResponse = "hi" + fbUsername + "need to open the account ? Which type Saving or Current Account ?"
 
-                return res.json({
+//                 return res.json({
+//                     speech: textResponse,
+//                     display: textResponse,
+//                     source : "greeting"
+//                 });
+//             }
+//             else {
+//                 return res.status(400).json({
+//                     status : {
+//                         code : 400,
+//                         errorType : "Error while binding the username"
+//                     }
+//                 });
+//             }
+//         });
+        
+            if (req.body.result.resolvedQuery == "hi") {
+                let textResponse = "hi, Siri need to open the account ? Which type Saving or Current Account ?"
+                 return res.json({
                     speech: textResponse,
                     display: textResponse,
                     source : "greeting"
                 });
             }
-            else {
-                return res.status(400).json({
-                    status : {
-                        code : 400,
-                        errorType : "Error while binding the username"
-                    }
-                });
-            }
-        });
-    }
+     }
 });
