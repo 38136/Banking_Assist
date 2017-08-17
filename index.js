ea877ec55d180ad73d2e094ba6d5e8b8766e91c8
38136 <31004566+38136@users.noreply.github.com>
2017-08-17 10:50:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get('\webhook', (res, req)=>{
-    if(req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === 'EAAEWihjxxmgBAGPW3iDD4t7lq0HCPGohnzmuXaclOZBX22hwCqAtfb7Y1vNibuSUtwyWeVZCnx8NsZC8szAuc6LkFbN7kc7fmWMfsmQQQdrm7up15YpN7sHDOblOLO3jnk8bCWVqp0MfFSanV5T89qdiqtElhnPFoiv9YZCzSQZDZD'){
-    console.log("validting webhook");
-        res.status(200).send(res.query['hub.challenge']);
+app.get('/webhook', (req, res)=>{
+    if(req.query['hub.mode'] === 'subscribe' && 
+       req.query['hub.verify_token'] === 'EAAEWihjxxmgBAGPW3iDD4t7lq0HCPGohnzmuXaclOZBX22hwCqAtfb7Y1vNibuSUtwyWeVZCnx8NsZC8szAuc6LkFbN7kc7fmWMfsmQQQdrm7up15YpN7sHDOblOLO3jnk8bCWVqp0MfFSanV5T89qdiqtElhnPFoiv9YZCzSQZDZD'){
+    console.log("validating webhook");
+        res.status(200).send(req.query['hub.challenge']);
     } else {
     console.log("Failed Valdation, check weather the token is match ?");
         res.sendStatus(403);
