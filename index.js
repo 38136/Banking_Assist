8f4e113672c4d2ea02a220d2b48f63a6dcbe824f
38136 <31004566+38136@users.noreply.github.com>
2017-08-18 09:36:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.post("/", function (req, res) {
     var sender_id = req.body.originalRequest.data.sender.id;
     var rec_id = req.body.originalRequest.data.recipient.id;
 
-    if (req.body.result.action == "input.welcome") {
+    if (req.body.result.action == "greeting") {
         if (req.body.result.resolvedQuery == "hi") {
             request({
                 uri: fburl + sender_id + "?access_token=" + FACEBOOK_ACCESS_TOKEN,
